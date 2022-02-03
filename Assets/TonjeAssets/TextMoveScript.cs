0ebf9346cb1f4aa06ec450ce291d474d874d3557
Tonje Røsnes <tonjerrr@gmail.com>
2022-02-03 14:48:37 +0100

    text, bg, movement script

---
+++ Assets/TonjeAssets/TextMoveScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMoveScript : MonoBehaviour
+{
+
+    public Transform startPos, endPos;
+    float speed = 2f;
+    public bool atEnd;
+
+    RectTransform rect;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+
+        if (!atEnd) transform.Translate(Vector2.down * speed * Time.deltaTime);
+        if (atEnd)
+        {
+            transform.localPosition = startPos.localPosition;
+            atEnd = false;
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "End")
+        {
+            atEnd = true;
+        }
+    }
+
+
+}
